f748e4a3adad6d70582444be6204b9ee1fa6a36f
cqc <285497458@qq.com>
2017-12-06 09:03:49 +0800

    first commit

---
+++ recommend/cf/user_base.py
@@ -0,0 +1,13 @@
+‘’‘
+read data from file(ratings.csv, [userId,movieId,rating,timestamp])
+build up the map: {uid: {movieId:rating}}
+also build the map: {moveId: [uid1, uid2]}
+
+change the {uid:{moverId:rating}} to the matrix/vector
+
+core of user-based cf
+calculate the sim between different users
+filter the users whose sim is larger than some value we set
+use those filtered users to predict the score for the movie
+sort the list
+’‘’
